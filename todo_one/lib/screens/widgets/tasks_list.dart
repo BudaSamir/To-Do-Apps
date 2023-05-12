a85025f0d3d5898a04d9137cb6692ce842e24a5a
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-05-12 19:26:36 +0300

    Created Tasks List and Passed into Tasks Widget

--- todo_one/lib/screens/widgets/tasks_list.dart
+++ todo_one/lib/screens/widgets/tasks_list.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:todo_one/models/task.dart';
 import 'package:todo_one/screens/widgets/task_title.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({
     super.key,
   });
 
   @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  // List contain The Tasks ( Task_model )
+  // created Objects from Class Task in a List, and give then values
+  List<Task> tasks = [
+    Task(taskTitle: "Study MVVM"),
+    Task(taskTitle: "Study Restful API's"),
+    Task(taskTitle: "Study Bloc"),
+  ];
+  @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
-        // TaskTile(),
-        // Divider(),
-        // TaskTile(),
-        // Divider(),
+      // TaskTile present the UI and Task instance present Data
+      // passing task instance value to tasktile variable
+      children: [
+        TaskTile(
+          taskTitle: tasks[0].taskTitle,
+          isChecked: tasks[0].isDone,
+        ),
+        const Divider(),
+        TaskTile(
+          taskTitle: tasks[1].taskTitle,
+          isChecked: tasks[1].isDone,
+        ),
+        const Divider(),
+        TaskTile(
+          taskTitle: tasks[2].taskTitle,
+          isChecked: tasks[2].isDone,
+        ),
+        const Divider(),
       ],
     );
   }
