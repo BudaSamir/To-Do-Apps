9226864d490ffab93e9b2a9caaeb42b0e51b5a6d
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-05-12 08:51:44 +0300

    Created Task Model

---
+++ todo_one/lib/models/task.dart
@@ -0,0 +1,15 @@
+class Task {
+  // Task Title ( Name of The Task)
+  final String name;
+  // The State of the Task Done or not
+  bool isDone;
+
+  // Building The instance with values of Variables
+  Task({required this.name, this.isDone = false});
+
+  // Change The State of The Task by The CheckBox, Done Or UnDone
+  void doneChange() {
+    // the isDone Variable equals the reverse Value of itself
+    isDone = !isDone;
+  }
+}
